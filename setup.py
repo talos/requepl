23b550c11dc4014c97ff3af7f526a2f2a2c3e258
talos <irving.krauss@gmail.com>
2011-12-08 23:27:05 -0500

    Added setup.py, now installs as executable.  Tweaked session switching and repl prompt.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='curlpl',
+      license='GPLv3',
+      version='0.0.4',
+      description='HTTP REPL with multiple sessions.',
+      author='John Krauss',
+      author_email='irving.krauss@gmail.com',
+      url='http://github.com/talos/curlpl',
+      scripts=['curlpl']
+      )
